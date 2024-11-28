62d910f097af955f098c983274aae97c85924024
Russell Poldrack <poldrack@gmail.com>
2024-11-28 07:45:02 -0800

    build in wait + download, clean up

--- src/ontology_learner/run_gpt4_batch.py
+++ src/ontology_learner/run_gpt4_batch.py
@@ -3,24 +3,28 @@ import os
 import argparse
 import json
 from pathlib import Path
+import time
+from gpt4_batch_utils import get_batch_results, save_batch_results
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     # create a cli to take in the batchfile
-    parser = argparse.ArgumentParser(
-        description='Run a batch of requests with GPT-4')
-    parser.add_argument('-b', '--batchfile', type=str, help='path to the batchfile')
+    parser = argparse.ArgumentParser(description="Run a batch of requests with GPT-4")
+    parser.add_argument("-b", "--batchfile", type=str, help="path to the batchfile")
+    parser.add_argument("-d", "--desc", type=str, help="description of the batch")
+    parser.add_argument(
+        "-o", "--outdir", type=str, help="output directory", default="batch_results"
+    )
+
     args = parser.parse_args()
 
     batchfile = Path(args.batchfile)
-    tracking_file = batchfile.parent / 'batch_tracking.json'
+    tracking_file = batchfile.parent / "batch_tracking.json"
     batchlabel = batchfile.stem
 
-    api_key = os.getenv('OPENAI')
+    api_key = os.getenv("OPENAI")
     client = OpenAI(api_key=api_key)
 
-    batch_input_file = client.files.create(file=open(batchfile, "rb"),
-                                           purpose="batch")
+    batch_input_file = client.files.create(file=open(batchfile, "rb"), purpose="batch")
 
     batch_input_file_id = batch_input_file.id
 
@@ -28,26 +32,39 @@ if __name__ == '__main__':
         input_file_id=batch_input_file_id,
         endpoint="/v1/chat/completions",
         completion_window="24h",
-        metadata={
-            "description": "annotation of PMC papers"
-        }
+        metadata={"description": args.desc},
     )
 
     # add the batch id to the tracking file
     if tracking_file.exists():
-        with open(tracking_file, 'r') as f:
+        with open(tracking_file, "r") as f:
             tracking_data = json.load(f)
     else:
         tracking_data = {}
 
     tracking_data[batchlabel] = {
-        'batchfile': args.batchfile,
-        'id': batch_metadata.id,
-        'status': batch_metadata.status,
-        'input_file_id': batch_input_file_id,
-        'output_file_id': batch_metadata.output_file_id
+        "batchfile": args.batchfile,
+        "id": batch_metadata.id,
+        "description": args.desc,
+        "status": batch_metadata.status,
+        "input_file_id": batch_input_file_id,
+        "output_file_id": batch_metadata.output_file_id,
     }
-    with open(tracking_file, 'w') as f:
+    with open(tracking_file, "w") as f:
         json.dump(tracking_data, f, indent=4)
 
     print(client.batches.retrieve(batch_metadata.id))
+
+    while client.batches.retrieve(batch_metadata.id).status != "completed":
+        print(client.batches.retrieve(batch_metadata.id).status)
+        time.sleep(60)
+    try:
+        os.system("say 'your program has finished'")
+    except:  # noqa: E722
+        pass
+
+    batch_results = get_batch_results(client, batch_metadata.id)
+    outdir = batchfile.parent.parent / args.outdir
+    outdir.mkdir(exist_ok=True, parents=True)
+
+    outfile = save_batch_results(batch_results, args.batchfile.stem, outdir)
