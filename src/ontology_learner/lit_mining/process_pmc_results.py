7e68a4dccbd8c9cf2b07816ddc3d8142292b1b7f
Russell Poldrack <poldrack@gmail.com>
2024-11-29 07:17:17 -0800

    extract paperResults class

--- src/ontology_learner/lit_mining/process_pmc_results.py
+++ src/ontology_learner/lit_mining/process_pmc_results.py
@@ -24,67 +24,12 @@ from collections import defaultdict
 from dataclasses import dataclass
 from llm_query.chat_client import ChatClientFactory
 from openai import OpenAI
+from paperResults import PaperResults
 api_key = os.environ.get("OPENAI")
 
 
 
 # %%
-@dataclass
-class PaperResults:
-    results_dir: Path
-    output_dir: Path
-    
-    def __init__(self, results_dir: Path, output_dir: Path):
-        self.results_dir = results_dir
-        self.output_dir = output_dir
-        self.constructs = []
-        self.tasks = []
-        self.conditions = {}
-        self.contrasts = {}
-        self.brain_regions = []
-        self.results_files = []
-
-    # get results files
-    def get_results_files(self):
-        self.results_files = list(self.results_dir.glob('*.json'))
-        self.results_files.sort()
-        print(f'found {len(self.results_files)} results files')
-
-    # load data files
-    def parse_results_files(self):
-
-        for filename  in self.results_files:
-            pmcid = filename.stem
-            with open(filename, 'r') as f:
-                paper_results = json.load(f)
-            if 'construct' in paper_results:
-                self.constructs.extend(paper_results['construct'])
-            if 'task' in paper_results:
-                self.tasks.extend(paper_results['task'])
-            # these are dicts
-            if 'condition' in paper_results:
-                for task, value in paper_results['condition'].items():
-                    if len(value) == 0:
-                        continue
-                    if task in self.conditions:
-                        self.conditions[task].extend(value)
-                    else:
-                        self.conditions[task] = value
-            if 'contrast' in paper_results:
-                for task, value in paper_results['contrast'].items():
-                    if len(value) == 0:
-                        continue
-                    if task in self.contrasts:
-                        self.contrasts[task].extend(value)
-                    else:
-                        self.contrasts[task] = value
-            if 'brain_region' in paper_results:
-                self.brain_regions.extend(paper_results['brain_region'])
-        # remove duplicates
-        self.constructs = list(set(self.constructs))
-        self.tasks = list(set(self.tasks))
-        self.brain_regions = list(set(self.brain_regions))
-
 
 
 # %%
