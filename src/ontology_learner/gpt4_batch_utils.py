1a46b4dbf738694f52288ab64097de2662a10870
Russell Poldrack <poldrack@gmail.com>
2024-12-05 08:14:52 -0800

    fix cancel code

--- src/ontology_learner/gpt4_batch_utils.py
+++ src/ontology_learner/gpt4_batch_utils.py
@@ -16,7 +16,7 @@ def list_completed_batches(client):
 def cancel_all_running_batches(client):
     batches = client.batches.list()
     for batch in batches:
-        if batch.status == 'running':
+        if batch.status == 'in_progress':
             print(f'cancelling batch {batch.id}')
             client.batches.cancel(batch.id)
 
