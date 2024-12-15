d36f4817361b21b1a073ab5fdbc1c3bb8e951261
Russell Poldrack <poldrack@gmail.com>
2024-12-15 08:16:07 -0800

    initial add

---
+++ src/ontology_learner/gpt4_direct/together_utils.py
@@ -0,0 +1,18 @@
+import json
+from together import Together
+
+
+def chat(messages, model="meta-llama/Llama-3.3-70B-Instruct-Turbo", 
+         seed=None, temperature=None, max_tokens=32000):
+    result = Together().chat.completions.create(
+        model=model,
+        messages=messages,
+        # response_format={"type": "json_object"},
+        seed=seed,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        )
+    try:
+        return json.loads(result.choices[0].message.content)
+    except Exception as e:
+        raise e
