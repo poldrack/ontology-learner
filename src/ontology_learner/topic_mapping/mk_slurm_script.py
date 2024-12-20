4a83207bcd6cc32b7d4ab30168a94d1c2d032636
Russ Poldrack <poldrack@gmail.com>
2024-12-20 10:39:12 -0800

    initial add

---
+++ src/ontology_learner/topic_mapping/mk_slurm_script.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+from pathlib import Path
+
+if __name__ == '__main__':
+
+    # read stub file
+    with open('bertopic.slurm_stub', 'r') as f:
+        stub = f.read()
+    
+    runfile = open('bertopic_jobs.slurm', 'w')
+    runfile.write(stub + '\n\n')
+    for n_neighbors in [25, 50, 100, 200]:
+        for min_cluster_size in [50, 100, 200]:
+            runfile.write(f'srun bertopic.sh {n_neighbors} {min_cluster_size}\n')
+    runfile.close()
